cb6f0cfd0dd34f29cc32df7191c6b84ad3a17596
Nguyen Tran <kevtran07@gmail.com>
2016-07-26 23:08:30 +0700

    Set up heroku creds and route to clearDB

--- config/database.php
+++ config/database.php
@@ -1,5 +1,9 @@
 <?php
-
+ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+ $host = $url["host"];
+ $username = $url["user"];
+ $password = $url["pass"];
+ $database = substr($url["path"], 1);
 return [
 
     /*
@@ -26,7 +30,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'mysql',
 
     /*
     |--------------------------------------------------------------------------
@@ -54,11 +58,10 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
