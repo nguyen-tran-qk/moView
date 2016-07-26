28602f062b853bb6571a77effe230fedddc2abdd
Nguyen Tran <kevtran07@gmail.com>
2016-07-26 23:59:11 +0700

    revert index blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,8 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     <!-- <link href="/images/favicon.png" rel="shortcut icon"> -->
-    {{HTML::style('styles/vendor.css',array(),true)}}
-    {{HTML::style('styles/app.css',array(),true)}}
-    <!-- <link rel="stylesheet" href="{!! asset('styles/vendor.css') !!}"> -->
-    <!-- <link rel="stylesheet" href="{!! asset('styles/app.css') !!}"> -->
+    <link rel="stylesheet" href="{!! asset('styles/vendor.css') !!}">
+    <link rel="stylesheet" href="{!! asset('styles/app.css') !!}">
     <title>moView</title>
     <meta name="csrf-token" content='<?php echo csrf_token(); ?>'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,10 +16,8 @@
   <div ui-view="main" >
       <!-- <div class="loading"></div> -->
   </div>
-<!-- <script src="{!! asset('scripts/vendor.js') !!}"></script> -->
-<!-- <script src="{!! asset('scripts/app.js') !!}"></script> -->
-    {{HTML::script('scripts/vendor.js',array(),true)}}
-    {{HTML::script('scripts/app.js',array(),true)}}
+<script src="{!! asset('scripts/vendor.js') !!}"></script>
+<script src="{!! asset('scripts/app.js') !!}"></script>
 
 {{--livereload--}}
 @if ( Config::get('app.debug') )
