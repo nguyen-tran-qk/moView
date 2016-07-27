b1648cb72b5c263ebb8aee88464a4d9b770194c8
Nguyen Tran <kevtran07@gmail.com>
2016-07-28 00:25:05 +0700

    config migrations, gitignore, gulpfile.js and composer.json

---
+++ database/migrations/2016_07_25_133128_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->longText('description');
+            $table->integer('duration');
+            $table->string('cast');
+            $table->string('director');
+            $table->date('dateReleased');
+            $table->integer('rating');
+            $table->string('trailer');
+            $table->string('poster');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('movies');
+    }
+}
