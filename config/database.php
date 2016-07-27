0e46ef00319fcdcde12961a6b44d8e94c3c79e4f
Nguyen Tran <kevtran07@gmail.com>
2016-07-27 08:08:30 +0700

    config database.php variables

--- config/database.php
+++ config/database.php
@@ -1,9 +1,20 @@
 <?php
- $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
- $host = $url["host"];
- $username = $url["user"];
- $password = $url["pass"];
- $database = substr($url["path"], 1);
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $host = '';
+    $username = '';
+    $password = '';
+    $database = '';
+ if (count($url) > 1) {
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+ } else {
+    $host = env('DB_HOST', 'localhost');
+    $username = env('DB_USERNAME', 'forge');
+    $password = env('DB_PASSWORD', '');
+    $database = env('DB_DATABASE', 'forge');
+ }
 return [
 
     /*
