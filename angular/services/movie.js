481c2ac547039f63ed7055f05401b567a1f5ce43
minhkhoi28 <minhkhoi28@yahoo.com>
2016-08-01 21:19:15 +0700

    MovieService added

---
+++ angular/services/movie.js
@@ -0,0 +1,45 @@
+(function() {
+  'use strict';
+
+  angular.module('MoviewServices', [])
+    .factory('MovieService', ['$http', '$q', function($http, $q) {
+      var movie,
+        data = {};
+      return {
+        getDataValue: function(name) {
+          return data[name];
+        },
+        getMovieList: function(callback, errorCallback) {
+          $http.get('/movies')
+            .success(function(res) {
+              if (callback) {
+                callback(res);
+              }
+            })
+            .error(function() {
+              if (errorCallback) {
+                errorCallback();
+              }
+            });
+        },
+        getMovieInfo: function(callback, errorCallback) {
+          $http.get('/movies')
+            .success(function(res) {
+              if (res && res.body) {
+                if (res.body.uid) {
+                  user = res.body;
+                }
+                if (callback) {
+                  callback();
+                }
+              }
+            })
+            .error(function(res) {
+              if (errorCallback) {
+                errorCallback();
+              }
+            });
+        }
+      };
+    }]);
+}());
