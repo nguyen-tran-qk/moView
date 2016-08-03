2cc2c2344c72d509a92f5af0b56dbc3a8a7e43a9
minhkhoi28 <minhkhoi28@yahoo.com>
2016-08-03 22:56:59 +0700

    user config

--- angular/controllers/movie.js
+++ angular/controllers/movie.js
@@ -2,12 +2,14 @@
   'use strict';
   angular.module('MoviewControllers')
     .controller('MovieController', ['$scope', '$sce', '$http', '$uibModal', '$stateParams', '$state', 'UserService', 'MovieService', function($scope, $sce, $http, $uibModal, $stateParams, $state, UserService, MovieService) {
+      if ($scope.user.role !== 32) {
+        $state.go('index.home');
+      }
       $scope.trustSrc = function(src) {
         return $sce.trustAsResourceUrl(src);
       };
       MovieService.getMovieInfo(parseInt($stateParams.id), function(res) {
         if (res && res.meta.code <= 200) {
-          console.log(res);
           $scope.movieDetail = res.body;
           if ($scope.movieDetail.trailer && $scope.movieDetail.trailer.indexOf('watch?v=') !== -1) {
             $scope.movieDetail.trailer = $scope.movieDetail.trailer.replace('watch?v=', 'embed/');
